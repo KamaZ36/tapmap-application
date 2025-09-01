30ee35ead99dc2b734340aa13bde1a3c715b57e0
KamaZ.36 <danila.skidan@mail.ru>
2025-09-01 10:41:44 +0300

    add updated at column to database and fix datetime

--- app/infrastructure/database/models/base.py
+++ app/infrastructure/database/models/base.py
@@ -1,7 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlalchemy import DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
+def get_datetime_utc_now() -> datetime:
+    datetime_now = datetime.now(tz=timezone.utc)
+    return datetime_now.replace(tzinfo=None)
+
+
 class BaseModel(DeclarativeBase): 
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        default=get_datetime_utc_now, 
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=get_datetime_utc_now,
+        onupdate=get_datetime_utc_now,
+        server_default=func.now(),
+        server_onupdate=func.now()
+    )
